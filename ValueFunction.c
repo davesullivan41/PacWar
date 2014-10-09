2c7c0e1bd8e1c5090553c5f849b3a24475ef3cb8
dave <davesullivan41@gmail.com>
2014-10-08 21:42:24 -0500

    Added quicksort to ValueFunction

--- ValueFunction.c
+++ ValueFunction.c
@@ -4,7 +4,9 @@
 
 #include "GeneGenerator.h"
 
-#define VF_SIZE 32
+#ifndef VF_SIZE
+#define VF_SIZE 16
+#endif
 
 struct GeneScore{
 	char *gene;
@@ -15,6 +17,36 @@ struct ValueFunction{
 	struct GeneScore *geneScores[VF_SIZE];
 };
 
+// creates a gene with score of zero
+struct GeneScore *createGeneScore(char *gene);
+
+// creates a pseudo-random "good" gene score
+struct GeneScore *createGoodGeneScore();
+
+// creates a pseudo-random "better" gene score
+struct GeneScore *createBetterGeneScore();
+
+// free memory of gene score
+void deleteGeneScore(struct GeneScore *geneScore);
+
+// create a value function with VF_SIZE "good" genes
+struct ValueFunction *createValueFunction();
+
+// free memory of value function
+void deleteValueFunction(struct ValueFunction *valueFunction);
+
+// set a specific valueFunction location to a specific geneScore
+void setValueFunctionGene(struct ValueFunction *valueFunction,struct GeneScore *geneScore,int index);
+
+// get the score of a certain gene
+struct GeneScore *getGeneScore(struct ValueFunction *valueFunction,int index);
+
+// get the gene of a specific genescore
+char *getGene(struct ValueFunction *valueFunction,int index);
+
+// return 1 if geneA has a higher score than B, -1 if its less, 0 if they're equal
+int compareGenes(struct GeneScore *geneScoreA,struct GeneScore *geneScoreB);
+//////////////////////////////////////////////////////////
 // creates a gene with score of zero
 struct GeneScore *createGeneScore(char *gene){
 	// allocate space for structure
@@ -33,8 +65,7 @@ struct GeneScore *createGeneScore(char *gene){
 }
 
 // creates a pseudo-random "good" gene score
-struct GeneScore *createGoodGeneScore()
-{
+struct GeneScore *createGoodGeneScore(){
 	char *a = malloc(sizeof(char)*50);
 	getGoodGene(a);
 	struct GeneScore *geneScore = createGeneScore(a);
@@ -43,8 +74,7 @@ struct GeneScore *createGoodGeneScore()
 }
 
 // creates a pseudo-random "better" gene score
-struct GeneScore *createBetterGeneScore()
-{
+struct GeneScore *createBetterGeneScore(){
 	char *a = malloc(sizeof(char)*50);
 	getBetterGene(a);
 	struct GeneScore *geneScore = createGeneScore(a);
@@ -60,8 +90,7 @@ void deleteGeneScore(struct GeneScore *geneScore){
 }
 
 // create a value function with VF_SIZE "good" genes
-struct ValueFunction *createValueFunction()
-{
+struct ValueFunction *createValueFunction(){
 	struct ValueFunction *valueFunction = malloc(sizeof(struct ValueFunction));
 	int i;
 	for(i=0;i<VF_SIZE;i++){
@@ -76,21 +105,66 @@ void deleteValueFunction(struct ValueFunction *valueFunction){
 }
 
 // set a specific valueFunction location to a specific geneScore
-void setValueFunctionGene(struct ValueFunction *valueFunction,struct GeneScore *geneScore,int index)
-{
+void setValueFunctionGene(struct ValueFunction *valueFunction,struct GeneScore *geneScore,int index){
 	valueFunction->geneScores[index] = geneScore;
 }
 
 // get the score of a certain gene
-struct GeneScore *getGeneScore(struct ValueFunction *valueFunction,int index)
-{
+struct GeneScore *getGeneScore(struct ValueFunction *valueFunction,int index){
 	return valueFunction->geneScores[index];
 }
 
 // get the gene of a specific genescore
-char *getGene(struct ValueFunction *valueFunction,int index)
-{
+char *getGene(struct ValueFunction *valueFunction,int index){
 	return valueFunction->geneScores[index]->gene;
+}
+
+// return 1 if geneA has a higher score than B, -1 if its less, 0 if they're equal
+int compareGenes(struct GeneScore *geneScoreA,struct GeneScore *geneScoreB){
+	if(geneScoreA->score > geneScoreB->score)
+	{
+		return 1;
+	}
+	else if(geneScoreA->score < geneScoreB->score)
+	{
+		return -1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+// swap gene i and gene j
+void swap(struct GeneScore **geneScore,int i,int j){
+	struct GeneScore *temp = geneScore[i];
+	geneScore[i] = geneScore[j];
+	geneScore[j] = temp;
+}
+
+// sort array using quicksort algorithm
+void geneSort(struct GeneScore **geneScores,int length){
+	if(length < 2){
+		return;
+	}
+	int i = 0;
+	int j;
+	for(j=1;j<length;j++)
+	{
+		// printf("Before compare %d\n",j);
+		if(compareGenes(geneScores[i],geneScores[j]) == 1){
+			printf("Before swaps %d\n",j);
+			swap(geneScores,i,i+1);
+			// printf("After first swap %d\n",j);
+			if(j != i+1){
+				swap(geneScores,i,j);
+			}
+			// printf("After second swap %d\n",j);
+			i++;
+		}
+	}
+	geneSort(&geneScores[0],i);
+	geneSort(&geneScores[i+1],length-i-1);
 }
 
 int main(){
@@ -112,6 +186,12 @@ int main(){
 		}
 	}
 
+	// print the score of every gene
+	for(i=0;i<VF_SIZE;i++)
+	{
+		printf("Gene %d total score: %d\n",i,getGeneScore(valueFunction,i)->score);
+	}
+	geneSort(valueFunction->geneScores,VF_SIZE);
 	// print the score of every gene
 	for(i=0;i<VF_SIZE;i++)
 	{
